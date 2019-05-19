18d9e53e080b4b2b75c6aa94548ffc3e46639bff
Mark Israel <nissimlidan@gmail.com>
2019-05-19 15:43:46 +0300

    Feat: Login page

---
+++ src/views/Login.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="full-height">
+        
+
+ <div id="Login">
+
+        <div class="Username"> <h1>LOGIN:</h1> </div>
+
+        <div class="Username"> <h2><input placeholder="Username" width="100px" height="12px"> </h2> </div>
+
+        <div class="Username"><h2> <input placeholder="Password" width="100px" height="12px">  </h2> </div>
+
+        <div class="Username"> <button> <h3> Login </h3> </button></div>
+
+        <div class="Username"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Facebook_logo_%28square%29.png/600px-Facebook_logo_%28square%29.png" width="25px" height="25px" align="left"> Login with Facebook</div>
+
+        <div class="Username"><img src="https://herevde.net/wp-content/uploads/2018/06/google-g-logo.jpg" width="25px" height="25px" align="left"> Login with Google</div>
+
+
+        </div>
+     
+      
+
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+#Login {
+
+                color:#251d1d;
+                width:400px;
+                margin-left: 800px;
+                margin-top: 250px;
+                align-items: center;
+
+
+}
+
+.Username{
+
+    padding: 8px;
+    display: flex;
+}
+
+p{
+
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+</style>
